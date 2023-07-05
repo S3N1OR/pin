6625ad4edaeb39d8cdb25a995eab2d48d864ee93
S3N1OR <126277562+S3N1OR@users.noreply.github.com>
2023-07-05 11:11:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,23 +7,19 @@ function myFunction(boxNumber) {
   navigator.clipboard.writeText(copyText.value);
 }
 
-// Получаем все поля ввода
 var inputs = document.querySelectorAll('.input');
 
-// Функция для добавления обработчиков событий
 function addInputEventHandlers(input, index) {
-  input.addEventListener('input', function() {
+  input.addEventListener('input', function () {
     localStorage.setItem('input_' + index, input.value);
   });
 
-  // Восстанавливаем значения полей ввода из локального хранилища
   var inputValue = localStorage.getItem('input_' + index);
   if (inputValue) {
     input.value = inputValue;
   }
 }
 
-// Добавляем обработчики событий для каждого поля ввода
 for (var i = 0; i < inputs.length; i++) {
   addInputEventHandlers(inputs[i], i);
 }
